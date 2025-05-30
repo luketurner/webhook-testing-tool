3624da4b6b37dbdf46b29733a8a6e84a268d934b
Luke Turner <github@luketurner.org>
2025-05-29 18:00:03 -0700

    add preliminary Request and Response types in code editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,4 +1,28 @@
-import Editor from "@monaco-editor/react";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import { useEffect } from "react";
+import { loader } from "@monaco-editor/react";
+
+// Note -- I'm getting errors when using the useMonaco hook, so using loader init instead.
+loader.init().then((monaco) => {
+  // TODO -- Proper types
+  monaco.languages.typescript.typescriptDefaults.addExtraLib(
+    `
+    declare interface Request {
+      url: string;
+      method: string;
+      body?: string;
+      params?: Record<string, string>;
+      query?: Record<string, string>;
+    }
+
+    declare interface Response {
+      body?: string;
+      headers: Record<string, string>;
+      status: number;
+    }
+    `
+  );
+});
 
 export interface CodeEditorProps {
   onChange: (value: string) => void;
