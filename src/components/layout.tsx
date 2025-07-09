e53eae060469798a6354cc466b6f5d8afc65985b
Luke Turner <github@luketurner.org>
2025-07-09 14:46:40 -0700

    expand width for main content

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,9 +9,9 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
       <AppSidebar />
       <SidebarInset>
         <SidebarTrigger className="m-1" />
-        <main className="max-w-4xl mx-auto p-4 w-full">
+        <div className="max-w-6xl mx-auto p-4 w-full">
           <div className="grid justify-items-stretch">{children}</div>
-        </main>
+        </div>
       </SidebarInset>
       <ManualSheet />
     </>
