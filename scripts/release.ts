a71a2b78f2d0128f6645f896d93b8d5d3d25868d
Luke Turner <github@luketurner.org>
2025-07-27 12:01:01 -0700

    switch build and release into two separate scripts

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,22 +1,8 @@
+/**
+ * Construcs a Github release, if appropriate.
+ */
+
 import { $ } from "bun";
+import packageJson from "../package.json";
 
-const targets = [
-  "bun-linux-x64",
-  "bun-linux-arm64",
-  "bun-linux-x64-musl",
-  "bun-linux-arm64-musl",
-  "bun-windows-x64",
-  "bun-darwin-x64",
-  "bun-darwin-arm64",
-];
-
-await $`bun run tailwind -- -m -i ./src/dashboard/index.css -o ./src/dashboard/index.compiled.css`;
-
-for (const target of targets) {
-  buildTarget(target);
-}
-
-async function buildTarget(target: string) {
-  console.log(`Building ${target}...`);
-  await $`bun build --define RELEASE=true --compile --minify --sourcemap src/server.ts --outfile dist/wtt-${target.replace("bun-", "")} --target ${target}`;
-}
+await $`gh release create v${packageJson.version} --verify-tag --notes-file CHANGELOG.md -- dist/*`;
