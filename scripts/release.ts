9cf89958547530e58c035604a1f6dbaa5e61e700
Luke Turner <github@luketurner.org>
2025-07-27 12:13:07 -0700

    update release script to publish release if it doesn't already exist

--- scripts/release.ts
+++ scripts/release.ts
@@ -3,7 +3,21 @@
  */
 
 import { $ } from "bun";
+import packageJson from "../package.json";
 
-const tag = $`git describe --exact-match --tags HEAD 2>/dev/null`;
+const tag = `v${packageJson.version}`;
 
-await $`gh release create ${tag} --verify-tag --notes-file CHANGELOG.md -- dist/*`;
+// check if the release already exists
+const releaseCheck = await $`gh release view ${tag}`.nothrow();
+if (!releaseCheck.text().includes("release not found")) {
+  if (releaseCheck.exitCode === 1) {
+    // Unknown error
+    console.error(releaseCheck.text());
+    process.exit(1);
+  }
+  // Release already exists
+  process.exit(0);
+}
+
+// release doesn't exist
+await $`gh release create ${tag} --verify-tag --notes-file CHANGELOG.md -- dist/wtt-*`;
