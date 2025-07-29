435f8d0647b3d6033355bf184fb61807d3f211c2
Luke Turner <github@luketurner.org>
2025-07-28 17:26:18 -0700

    log warnings when using insecure defaults in production (but don't throw errors)

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,9 @@ export const DEV = NODE_ENV === "development";
 export const PROD = NODE_ENV === "production";
 export const TEST = NODE_ENV === "test";
 
+if (!process.env.BETTER_AUTH_SECRET && PROD)
+  console.warn("WARNING: Using default BETTER_AUTH_SECRET. You should set this to a secret value.");
+
 export const ADMIN_USERNAME =
   process.env.WTT_ADMIN_USERNAME || "admin@example.com";
 
@@ -17,7 +20,7 @@ export const DB_FILE = TEST
   : process.env.WTT_DB_FILE || join(DATA_DIR, "data.sqlite");
 
 if (!process.env.WTT_ADMIN_PASSWORD && PROD)
-  throw new Error("Must specify WTT_ADMIN_PASSWORD");
+  console.warn("WARNING: Using default password for admin dashboard. You should set WTT_ADMIN_PASSWORD to a secret value.");
 export const ADMIN_PASSWORD = process.env.WTT_ADMIN_PASSWORD || "admin123";
 
 export const WEBHOOK_PORT = parseInt(
